d1775fe338ecb6d7bb82dfecd8e5fc31f43956f7
david-leverenz <david.leverenz@gmail.com>
2023-07-13 20:16:21 -0500

    update thought working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,37 +1,51 @@
 const Thought = require('../models/Thought');
 
 module.exports = {
-  // Get all thoughts
-  async getThoughts(req, res) {
-    try {
-      const thoughtData = await Thought.find();
-      res.json(thoughtData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
-   // Create a new thought.
-   async createThought(req, res) {
-    try {
-        const thoughtData = await Thought.create(req.body);
-        res.json(thoughtData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-},
-  // Get a single thought.
-  async getSingleThought(req, res) {
-    try {
-      const thoughtData = await Thought.findOne({ _id: req.params.thoughtId })
-        .select('-__v');
+    // Get all thoughts
+    async getThoughts(req, res) {
+        try {
+            const thoughtData = await Thought.find();
+            res.json(thoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Create a new thought.
+    async createThought(req, res) {
+        try {
+            const thoughtData = await Thought.create(req.body);
+            res.json(thoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Get a single thought.
+    async getSingleThought(req, res) {
+        try {
+            const thoughtData = await Thought.findOne({ _id: req.params.thoughtId })
+                .select('-__v');
 
-      if (!thoughtData) {
-        return res.status(404).json({ message: 'No thought with that ID' });
-      }
+            if (!thoughtData) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
 
-      res.json(thoughtData);
-    } catch (err) {
-      res.status(500).json(err);
+            res.json(thoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Update a thought.
+    async updateThought(req, res) {
+        try {
+            const thoughtData = await Thought.findByIdAndUpdate({ _id: req.params.thoughtId },{$set: req.body }, {new: true, runValidators: true});
+            
+       
+            if (!thoughtData) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.status(200).json(thoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
-  },
-    }
+}
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,7 +3,7 @@ const {
   getThoughts,
   getSingleThought,
   createThought,
-  // updateThought,
+  updateThought,
   // deleteThought,
   // addThoughtReaction,
   // removeThoughtReaction,
@@ -13,8 +13,8 @@ const {
 router.route('/').get(getThoughts).post(createThought);
 
 // // /api/thoughts/:thoughtId
-// router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
-router.route('/:thoughtId').get(getSingleThought)
+// router.route('/:thoughtId').delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought)
 
 // // /api/thoughts/:thoughtId/reactions
 // router.route('/:thoughtId/reactions').post(addThoughtReaction);
